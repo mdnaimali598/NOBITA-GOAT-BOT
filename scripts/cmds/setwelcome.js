6433063fcd0dbc87b15e86b62e7e47e866b93987
mdnaimali598 <nahidulislamnaim1435@gmail.com>
2026-02-21 00:47:47 +0600

    Update setwelcome.js

--- scripts/cmds/setwelcome.js
+++ scripts/cmds/setwelcome.js
@@ -28,7 +28,7 @@ module.exports = {
 					+ "\n\n   Ví dụ:"
 					+ "\n    {pn} file reset: xóa gửi file",
 				attachment: {
-					[`${__dirname}/assets/guide/setwelcome/setwelcome_vi_1.png`]: "https://i.ibb.co/vd6bQrW/setwelcome-vi-1.png"
+					[`${__dirname}/assets/guide/setwelcome/setwelcome_vi_1.png`]: "https://files.catbox.moe/prjtqp.gif"
 				}
 			},
 			en: {
